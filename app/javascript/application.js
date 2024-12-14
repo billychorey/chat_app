29afa0f75e04075f01a50ee5c825592a230e549a
Billy <billychorey@gmail.com>
2024-12-13 20:23:06 -0500

    dummy react added

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,18 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-import "@hotwired/turbo-rails"
-import "controllers"
-import Rails from "@rails/ujs"
-Rails.start()
+import "@hotwired/turbo-rails";
+import "controllers";
+import Rails from "@rails/ujs";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/App";
+
+// Ensure Rails UJS is initialized
+Rails.start();
+
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.getElementById("react-root");
+  if (container) {
+    const root = createRoot(container);
+    root.render(<App />);
+  }
+});
---
+++ app/javascript/components/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <h1>Hello, React with Rails 7!</h1>;
+};
+
+export default App;
